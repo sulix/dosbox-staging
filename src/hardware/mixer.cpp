fd91212614012baf7c784a4a89681d323abed5d0
Kirk <kklobe@gmail.com>
2021-09-26 19:28:53 -0500

    Increase queue send threshold

--- src/hardware/mixer.cpp
+++ src/hardware/mixer.cpp
@@ -57,7 +57,7 @@
 
 static constexpr int MIXER_SSIZE = sizeof(MixerFrame);
 static constexpr int MIXER_MIN_NEEDED = 0;
-static constexpr int MIXER_SEND_QUEUE_THRESHOLD_MS = 10;
+static constexpr int MIXER_SEND_QUEUE_THRESHOLD_MS = 50;
 static constexpr int MIXER_MAX_LATENCY_MS = 100;
 static constexpr int MIXER_MAX_SAMPLE_RATE = 49716;
 static constexpr int MIXER_MAX_OUTPUT_BYTES =
