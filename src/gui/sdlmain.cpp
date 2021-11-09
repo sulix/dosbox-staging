3c05aff45dcce3c14d6157ea791cbb8c0a9567e6
kcgen <kcgen@users.noreply.github.com>
2021-11-08 17:37:32 -0800

    Add surface rendering to window-resize criteria
    This is needed because the surface renderer doesn't
    scale content. So this ensures the window is resized
    to make the content on mode changes.
    
    Without it, resizing from a higher resolution down to
    a lower-resolution would produce a small image
    without a large black region.

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -863,8 +863,10 @@ static SDL_Window *SetWindowMode(SCREEN_TYPES screen_type,
 
 		// Adjust the window dimensions if our window isn't sized yet or
 		// we're in PP mode
-		if (window_is_too_small || (sdl.scaling_mode == SCALING_MODE::PERFECT &&
-		                            window_dimensions_not_exact)) {
+		if (window_is_too_small ||
+		    (window_dimensions_not_exact &&
+		     (sdl.scaling_mode == SCALING_MODE::PERFECT ||
+		      sdl.desktop.type == SCREEN_SURFACE))) {
 			SDL_SetWindowSize(sdl.window, width, height);
 		}
 		// If we're switching down from fullscreen, then it will use the set window size
